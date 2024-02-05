aac89e3db995363ea7f47e89c8f389fd9c5c73f8
loboxos <vcristian528@gmail.com>
2024-02-05 13:15:33 -0300

    2 horas + cree el diseño de la calculadora - agregar scripts para las acciones de los botones *ningun impedimento

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -1,18 +1,63 @@
 <script>
-	export let name;
+	import Keypad from "./keypad.svelte";
+	let entrada = ""
+	let total = 0
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Calculadora basica</h1>
+
+	<div class="display">
+		<span>{entrada}</span>
+	</div>
+<div class="input-pad">
+
+	<Keypad tipoBoton ="borrar">C</Keypad>
+	<Keypad tipoBoton ="borrar">CE</Keypad>
+	<Keypad tamanio={2} tipoBoton ="historial">Hist</Keypad>
+
+	<Keypad>7</Keypad>
+	<Keypad>8</Keypad>
+	<Keypad>9</Keypad>
+	<Keypad tipoBoton ="operador">/</Keypad>
+
+	<Keypad>4</Keypad>
+	<Keypad>5</Keypad>
+	<Keypad>6</Keypad>
+	<Keypad tipoBoton ="operador">*</Keypad>
+
+
+	<Keypad>1</Keypad>
+	<Keypad>2</Keypad>
+	<Keypad>3</Keypad>
+	<Keypad tipoBoton ="operador">-</Keypad>
+
+	<Keypad>0</Keypad>
+	<Keypad>.</Keypad>
+	<Keypad tipoBoton ="operador">=</Keypad>
+	<Keypad tipoBoton ="operador">+</Keypad>
+	
+</div>
+	
 </main>
 
 <style>
+.display {
+	max-width: 440px;
+	margin: auto;
+    margin-bottom: 10px;
+	background-color: rgb(108, 175, 125);
+		border: 1px solid #000000;
+		padding: 31px;
+		margin-bottom: 10px;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		border-radius: 40px;
 		margin: 0 auto;
+		background-color: aqua;
 	}
 
 	h1 {
@@ -21,10 +66,13 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+   .input-pad {
+	max-width: 500px;
+	margin: auto;
+	display: grid;
+	grid-template-columns: repeat(4,1fr);
+	grid-template-rows: repeat(5,1fr);
+	gap:10px;
+   }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ front-end/src/keypad.svelte
@@ -0,0 +1,33 @@
+<script>
+ export let tamanio = 1;
+ export let tipoBoton = 'numero';
+function getColor (){
+    switch (tipoBoton){
+        case "numero":
+            return "#515050";
+        case "operador":
+            return "#7B7676"
+        case "borrar":
+            return "#EA3B3B"
+        case "historial":
+            return "#1F201F"
+        default:
+         return "#fff";
+    }
+}
+</script>
+<button class = "key-pad" style="grid-column:span {tamanio};background-color:{getColor()}; color:white">
+<slot></slot>
+</button>
+
+<style>
+    .key-pad {
+      border: 1px solid #515050;
+      padding: 22px 20px;
+      margin: 0;
+      font-size: 16px;
+    }
+
+    
+</style>
+
