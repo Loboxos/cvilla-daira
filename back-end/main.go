2620419328c1cdfc6c057ff288ea89f11e9e8cd3
loboxos <vcristian528@gmail.com>
2024-02-05 10:07:10 -0300

    30min +funcion para las operaciones de de la calculadora y validacion de operandos - creacion del servidor web para el api rest y el historial * no hay impedimento

--- back-end/main.go
+++ back-end/main.go
@@ -8,9 +8,37 @@ type Operacion struct {
 	Op1 int
 	Op2 int
 	Op  string
-	res int
+	Res int
 }
 
 func main() {
 	fmt.Println("Hola back-end")
+
+	op := Operacion{Op1: 100, Op2: 15, Op: "+"}
+	calculadora(&op)
+	fmt.Println(op.Res)
+}
+
+func calculadora(op *Operacion) {
+
+	if !validarRango(op.Op1) || !validarRango(op.Op2) {
+		fmt.Println("Los operandos son numeros de mas de 2 digitos")
+		return
+	}
+
+	switch op.Op {
+	case "+":
+		op.Res = op.Op1 + op.Op2
+	case "-":
+		op.Res = op.Op1 - op.Op2
+	case "/":
+		op.Res = op.Op1 / op.Op2
+	case "*":
+		op.Res = op.Op1 * op.Op2
+	default:
+		fmt.Println("fin switch")
+	}
+}
+func validarRango(numero int) bool {
+	return numero >= 0 && numero <= 99
 }
