4a564c2867ecf18aaae349cb634eec89d9e705da
Loboxos <vcristian528@gmail.com>
2024-02-06 16:19:12 -0300

    2 horas +condiciones para digitos ,historial y resultados de operaciones en el display - mejorar detalles de acuerdo al back y manejo de errores * no hay impedimentos

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -2,18 +2,17 @@
 	import Keypad from "./keypad.svelte";
 	let entrada = "";
 	let entrada2 ="";
-	let operandoActual = ""; 
 	let operacion ="";
 	let b=0;
-	let total = 0;
-
+    let total=0;
+	let historial = [];
 	function agregarDigito(digito) {
 		console.log(digito)
-    if (entrada.length < 2) {
+    if (entrada.length < 2 ) {
       entrada += digito;
 	  console.log(entrada)
     }
-	if (b === 1){
+	if (b === 1 && entrada2.length < 2) {
 		if (entrada2.length < 2) {
 			entrada2 += 1;
             entrada += digito;
@@ -53,13 +52,29 @@
           const result = await response.json();
           total = result.res;
           entrada = total.toString(); // Actualiza la entrada con el resultado
-        } else {
+		  historial.push(data);
+		} else {
           console.error('Error en la solicitud:', response.statusText);
         }
 } catch (error) {
         console.error('Error en la solicitud:', error.message);
       }}}
-
+	  const borrar = () => {
+		entrada = ""
+	}
+	async function obtenerHistorial() {
+    try {
+      const response = await fetch('http://localhost:8000/historial');
+      if (response.ok) {
+        historial = await response.json();
+        console.log('Historial:', historial);
+      } else {
+        console.error('Error al obtener el historial:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al obtener el historial:', error.message);
+    }
+  }
 </script>
 
 <main>
@@ -68,28 +83,32 @@
 	<div class="display">
 		<span>{entrada}</span>
 	</div>
+	<div class="historial">
+        {#each historial as operacion, i}
+            <div>{operacion.op1} {operacion.op} {operacion.op2} = {operacion.res}</div>
+        {/each}
+    </div>
 <div class="input-pad">
-	<Keypad tipoBoton ="borrar" on:click={() => entrada = ""}>C</Keypad>
-	<Keypad tipoBoton ="borrar" on:click={() => { entrada = ""; operandoActual = ""; operacion = ""; total = 0; }}>CE</Keypad>
-	<Keypad tamanio={2} tipoBoton ="historial" on:click={() => alert("No implementado")}>Hist</Keypad>
+	<Keypad tamanio={2} tipoBoton ="borrar" clicked={borrar}>C</Keypad>
+	<Keypad tamanio={2} tipoBoton ="historial" clicked={obtenerHistorial}>Hist</Keypad>
 
 <Keypad clicked={() => agregarDigito(7)}>7</Keypad>
 <Keypad clicked={() => agregarDigito(8)}>8</Keypad>
 <Keypad clicked={() => agregarDigito(9)}>9</Keypad>
-<Keypad clicked={() => agregarOperador("/")}>/</Keypad>
+<Keypad clicked={() => agregarOperador("/")} tipoBoton ="operador">/</Keypad>
 
 	<Keypad clicked={() => agregarDigito(4)}>4</Keypad>
 <Keypad clicked={() => agregarDigito(5)}>5</Keypad>
 <Keypad clicked={() => agregarDigito(6)}>6</Keypad>
-<Keypad clicked={() => agregarOperador("*")}>*</Keypad>
+<Keypad clicked={() => agregarOperador("*")} tipoBoton ="operador">*</Keypad>
 
 
 	<Keypad clicked={() => agregarDigito(1)}>1</Keypad>
 <Keypad clicked={() => agregarDigito(2)}>2</Keypad>
 <Keypad clicked={() => agregarDigito(3)}>3</Keypad>
-<Keypad clicked={() => agregarOperador("-")}>-</Keypad>
+<Keypad clicked={() => agregarOperador("-")} tipoBoton ="operador">-</Keypad>
 
-	<Keypad>0</Keypad>
+<Keypad clicked={() => agregarDigito(0)}>0</Keypad>
 	<Keypad>.</Keypad>
 	<Keypad clicked={realizarCalculo} tipoBoton ="operador">=</Keypad>
 	<Keypad clicked={() => agregarOperador("+")} tipoBoton ="operador">+</Keypad>
