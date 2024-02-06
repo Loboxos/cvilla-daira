8bd7551eb08c49fe1c7387cfe831113dca10587f
Loboxos <vcristian528@gmail.com>
2024-02-06 16:20:24 -0300

    2 horas + funcionalidad historial - manejo de errores *no hay impedimentos

--- back-end/main.go
+++ back-end/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 type Operacion struct {
@@ -16,30 +17,37 @@ type Operacion struct {
 	Res int    `json:"res"`
 }
 
+var historial []Operacion
+
 func main() {
 	fmt.Println("Hola back-end")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/calcular", calculadoraHandler).Methods("POST")
 	router.HandleFunc("/inicio", inicioHand).Methods("GET")
+	router.HandleFunc("/historial", obtenerHistorial).Methods("GET") // Agregar ruta para historial
 
 	//router.PathPrefix("/").Handler(http.FileServer(http.Dir("../front-end/public")))
+	handler := cors.Default().Handler(router)
 
 	http.Handle("/", router)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", handler)
 
 	//op := Operacion{Op1: 100, Op2: 15, Op: "+"}
 	//calculadora(&op)
 	//fmt.Println(op.Res)
 }
+
 func inicioHand(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hola desde el back")
 }
 func calculadoraHandler(w http.ResponseWriter, r *http.Request) {
 	//io.WriteString(w, "Hola mundo")
+	fmt.Println("hola estoy aqui")
 	var op Operacion
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&op)
+	fmt.Println(decoder)
 	if err != nil {
 		http.Error(w, "Error al decodificar la solicitud JSON", http.StatusBadRequest)
 		return
@@ -51,7 +59,22 @@ func calculadoraHandler(w http.ResponseWriter, r *http.Request) {
 	calculadora(&op)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(op)
+	historial = append(historial, op)
+}
+func obtenerHistorial(w http.ResponseWriter, r *http.Request) {
+	historialJSON, err := json.Marshal(historial)
+	if err != nil {
+		http.Error(w, "Error al convertir el historial a JSON", http.StatusInternalServerError)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err = w.Write(historialJSON)
+	if err != nil {
+		http.Error(w, "Error al escribir el historial JSON en la respuesta", http.StatusInternalServerError)
+		return
+	}
 }
 func calculadora(op *Operacion) {
 
